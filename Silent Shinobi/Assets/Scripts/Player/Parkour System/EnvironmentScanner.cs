ec117a31166f102d6da3776462a1cd1b2395131c
zZero111 <Charliemarshall1107@gmail.com>
2026-04-12 16:17:39 +0100

    RayCast Environment Scanner

---
+++ Silent Shinobi/Assets/Scripts/Player/Parkour System/EnvironmentScanner.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class EnvironmentScanner : MonoBehaviour
+{
+    [SerializeField] Vector3 forwardRayOffset = new Vector3(0, 0.25f, 0);
+    [SerializeField] float forwardRayLength = 0.8f;
+    [SerializeField] float heightRayLength = 5;
+    [SerializeField] LayerMask obstacleLayer;
+
+    public ObstacleHitData ObstacleCheck()
+    {
+        var hitData = new ObstacleHitData();
+
+        var forwardOrigin = transform.position + forwardRayOffset;
+        hitData.forwardHitFound = Physics.Raycast(forwardOrigin, transform.forward, out hitData.forwardHit, forwardRayLength, obstacleLayer);
+
+        Debug.DrawRay(forwardOrigin, transform.forward * forwardRayLength, (hitData.forwardHitFound) ? Color.red : Color.white);
+
+        if (hitData.forwardHitFound)
+        {
+            var heightOrigin = hitData.forwardHit.point + Vector3.up * heightRayLength;
+            hitData.heightHitFound = Physics.Raycast(heightOrigin, Vector3.down, out hitData.heightHit, heightRayLength, obstacleLayer);
+
+            Debug.DrawRay(heightOrigin, Vector3.down * heightRayLength, (hitData.heightHitFound) ? Color.red : Color.white);
+        }
+
+        return hitData;
+    }
+}
+
+public struct ObstacleHitData
+{
+    public bool forwardHitFound;
+    public bool heightHitFound;
+    public RaycastHit forwardHit;
+    public RaycastHit heightHit;
+}
---
+++ Silent Shinobi/Assets/Scripts/Player/Parkour System/ParkourController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ParkourController : MonoBehaviour
+{
+    EnvironmentScanner environmentScanner;
+
+    private void Awake()
+    {
+        environmentScanner = GetComponent<EnvironmentScanner>();
+    }
+
+    private void Update()
+    {
+        var hitData = environmentScanner.ObstacleCheck();
+        if (hitData.forwardHitFound)
+        {
+            Debug.Log("Obstacle Found " + hitData.forwardHit.transform.name);
+        }
+    }
+}
