130cad7495164546e3416f427586afabf6a901f2
CharFarn <charliemarshall1107@gmail.com>
2026-04-27 11:55:46 +0100

    Enemy pathing

---
+++ Silent Shinobi/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public int targetPoint;
+    public float speed;
+    void Start()
+    {
+        targetPoint = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == patrolPoints[targetPoint].position)
+        {
+            increaseTargetInt();
+        }
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+    }
+
+    void increaseTargetInt()
+    {
+        targetPoint++;
+        if (targetPoint >= patrolPoints.Length)
+        {
+            targetPoint = 0;
+        }
+    }
+}
